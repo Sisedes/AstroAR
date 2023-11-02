a3811c160634fe5e7974aa6283026a297fc1ab61
Kaan Toraman <kaan03tor@gmail.com>
2023-11-03 02:12:16 +0300

    Touchable 3D objects with messages

---
+++ AR Project/Assets/BaktginiAlgilama/ClickObject.cs
@@ -0,0 +1,105 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ClickObject : MonoBehaviour
+{
+    public GameObject cube;
+
+    [Header("MessageBox")]
+    [SerializeField] Transform SectionInfo;
+    Vector3 desiredScale = Vector3.zero;
+    const float speed = 6f;
+
+    public Transform[] allSectionInfos;
+    private void Start()
+    {
+        foreach (var hehe in allSectionInfos)
+        {
+            hehe.localScale = Vector3.Lerp(hehe.localScale, Vector3.zero, Time.deltaTime * speed);
+        }
+    }
+    
+    void Update()
+    {
+        //MessageBox shits
+        SectionInfo.localScale = Vector3.Lerp(SectionInfo.localScale, desiredScale, Time.deltaTime * speed);
+
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                Ray ray = Camera.main.ScreenPointToRay(touch.position);
+                RaycastHit hehe;
+
+                if (Physics.Raycast(ray, out hehe, 100))
+                {
+                    cube = hehe.transform.gameObject;
+                    SectionInfo = hehe.transform.GetChild(0);
+                    Debug.Log(hehe.transform.gameObject.name);
+                }
+
+                if (cube == GetClickedObject(out RaycastHit hit))
+                {
+                    OpenInfo();
+                }
+            }
+            else if (touch.phase == TouchPhase.Ended)
+            {
+                CloseInfo();
+            }
+        }
+
+    }
+
+
+
+    GameObject GetClickedObject(out RaycastHit hit)
+
+    {
+
+        GameObject target = null;
+
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray.origin, ray.direction * 10, out hit))
+
+        {
+
+            if (!isPointerOverUIObject()) { target = hit.collider.gameObject; }
+
+        }
+
+        return target;
+
+    }
+
+    private bool isPointerOverUIObject()
+
+    {
+
+        PointerEventData ped = new PointerEventData(EventSystem.current);
+
+        ped.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+
+        List<RaycastResult> results = new List<RaycastResult>();
+
+        EventSystem.current.RaycastAll(ped, results);
+
+        return results.Count > 0;
+
+    }
+
+     void OpenInfo()
+    {
+        desiredScale = Vector3.one;
+    }
+     void CloseInfo()
+    {
+        desiredScale = Vector3.zero;
+    }
+
+}
