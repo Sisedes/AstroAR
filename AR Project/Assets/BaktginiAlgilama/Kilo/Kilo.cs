beefb169c712c5a2d88a9f8f664574b3e345fea0
Kaan Toraman <kaan03tor@gmail.com>
2023-11-09 19:05:03 +0300

    kilo hesaplama
    .

--- AR Project/Assets/BaktginiAlgilama/Kilo/Kilo.cs
+++ AR Project/Assets/BaktginiAlgilama/Kilo/Kilo.cs
@@ -1,18 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Kilo : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public TMP_InputField kiloInput;
+    public TextMeshProUGUI kiloOutput;
+    public float katsayý;
+
+    private void Update()
     {
-        
+       kiloOutput.text = ((Convert.ToInt32(kiloInput.text)/9.81f) * katsayý).ToString()+" KG";
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
